8f9561d4b0c3b02bf6bfab5f5516f83868b865b4
runmole <therunmole@gmail.com>
2023-10-13 17:11:08 +0300

    удалил лишние импорты

--- src/main/java/web/service/CarServicelmpl.java
+++ src/main/java/web/service/CarServicelmpl.java
@@ -1,9 +1,7 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import web.model.Car;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
